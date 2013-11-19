8c1872bf07bf7529a23b88437e6fed47da404207
Cedric Veilleux <veilleux.cedric@gmail.com>
2013-11-19 14:21:25 -0500

    Refactored code to have a clearer flow for the gateway mode (on init) and the normal login. Fix infinite redirection loop issue.

--- wpcas.php
+++ wpcas.php
@@ -85,13 +85,30 @@ add_filter('show_password_fields', array('wpCAS', 'show_password_fields'));
 
 class wpCAS {
 
+  /**
+   * init action: If gateway mode is enabled, attempt CAS auto-login.
+   */
   function init() {
     global $wpcas_options;
 
-    // gateway mode support
-    if ($wpcas_options['gateway_mode']) {
-      wpCAS::authenticate(true);
+    if (!$wpcas_options['gateway_mode']) {
+      return;
     }
+
+    // user is already logged in wp
+    if (get_current_user_id() > 0) {
+      return;
+    }
+
+    if (!phpCAS::isAuthenticated()) {
+      // Ask phpCas to attempt gateway mode login
+      phpCAS::checkAuthentication();
+      return;
+    }
+
+    // User is phpCas authenticated, ensure it is also logged in wp
+    wpCAS::finish_login();
+
   }
 
 	/*
@@ -99,7 +116,7 @@ class wpCAS {
 	 (the script dies there if login was unsuccessful)
 	 If the user is not provisioned, 'provision_user_function' is called
 	*/
-	function authenticate($gatewayMode=false) {
+	function authenticate() {
 		global $wpcas_options, $cas_configured;
 
 		if ( !$cas_configured )
@@ -107,67 +124,68 @@ class wpCAS {
 
 		if( !phpCAS::isAuthenticated() ){
 		  // hey, authenticate
-		  if ($gatewayMode) {
-		    phpCAS::checkAuthentication();
-		    return;
-
-		  } else {
-		    phpCAS::forceAuthentication();
-		    die();
-
-		  }
+	    phpCAS::forceAuthentication();
+	    die();
 		}
 
-		if ($gatewayMode && get_current_user_id() > 0) {
-			// user is already logged
-			return;
+		// user is already logged in wp
+		if (get_current_user_id() > 0) {
+		  return;
 		}
 
-		// CAS was successful
-		$user = get_userdatabylogin( phpCAS::getUser() );
-
-		if (!$user) {
-
-		  // the CAS user _does_not_have_ a WP account
-		  if (!empty($wpcas_options['provision_user_function']) && function_exists( $wpcas_options['provision_user_function']))
-		    $user = call_user_func($wpcas_options['provision_user_function'], phpCAS::getUser());
-
-		  if (!$user) {
-		    // There is no automated user provisioning or user provisioning refused to create a user
-		    die( __( 'you do not have permission here', 'wpcas' ));
-		  }
-
-		}
-
-	  // user exists, complete the login:
-
-		// Allow custom user profile sync
-		if (!empty($wpcas_options['sync_user_function']) && function_exists( $wpcas_options['sync_user_function']))
-			call_user_func($wpcas_options['sync_user_function'], phpCAS::getUser(), phpCAS::getAttributes());
-
-		// reload the user after the sync
-		$user = get_userdatabylogin( phpCAS::getUser() );
-
-		if (!$user) {
-		  // The sync deleted the user. Fail.
-		  die( __( 'you do not have permission here', 'wpcas' ));
-		}
-
-		wp_set_auth_cookie( $user->ID );
-
-		// Allow other plugins to act on user login
-		do_action('wp_login', $user->user_login, $user);
-
-		if( isset( $_GET['redirect_to'] )){
-			wp_redirect( preg_match( '/^http/', $_GET['redirect_to'] ) ? $_GET['redirect_to'] : site_url( $_GET['redirect_to'] ));
-			die();
-		}
-
-		wp_redirect( $_SERVER["REQUEST_URI"] );
-		die();
+		wpCas::finish_login();
 
 	}
 
+	function finish_login() {
+	  // CAS was successful
+	  $user = get_userdatabylogin( phpCAS::getUser() );
+
+	  if (!$user) {
+
+	    // the CAS user _does_not_have_ a WP account
+	    if (!empty($wpcas_options['provision_user_function']) && function_exists( $wpcas_options['provision_user_function']))
+	      $user = call_user_func($wpcas_options['provision_user_function'], phpCAS::getUser());
+
+	    if (!$user) {
+	      // There is no automated user provisioning or user provisioning refused to create a user
+	      die( __( 'you do not have permission here', 'wpcas' ));
+	    }
+
+	  }
+
+	  // user exists, complete the login:
+
+	  // Allow custom user profile sync
+	  if (!empty($wpcas_options['sync_user_function']) && function_exists( $wpcas_options['sync_user_function']))
+	    call_user_func($wpcas_options['sync_user_function'], phpCAS::getUser(), phpCAS::getAttributes());
+
+	  // reload the user after the sync
+	  $user = get_userdatabylogin( phpCAS::getUser() );
+
+	  if (!$user) {
+	    // The sync deleted the user. Fail.
+	    die( __( 'you do not have permission here', 'wpcas' ));
+	  }
+
+	  wp_set_auth_cookie( $user->ID );
+
+	  // Allow other plugins to act on user login
+	  do_action('wp_login', $user->user_login, $user);
+
+	  if( isset( $_GET['redirect_to'] )){
+	    wp_redirect( preg_match( '/^http/', $_GET['redirect_to'] ) ? $_GET['redirect_to'] : site_url( $_GET['redirect_to'] ));
+	    die();
+	  }
+
+	  if (strstr($_SERVER["REQUEST_URI"], 'wp-login.php') !== false) {
+	    wp_redirect( site_url( '/' ) );
+	    die();
+	  }
+
+	  wp_redirect( $_SERVER["REQUEST_URI"] );
+	  die();
+	}
 
 	// hook CAS logout to WP logout
 	function logout() {
